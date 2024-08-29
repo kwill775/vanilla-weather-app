2452ae38e66828427ac46c7c9b870486327543a9
kwill775 <spn775@yahoo.com>
2024-08-28 21:14:32 -0700

    Added search function using api

--- src/app.js
+++ src/app.js
@@ -1,10 +1,27 @@
+function changeWeather(response) {
+  let tempElement = document.querySelector("#weather-app-temp");
+  let temperature = Math.round(response.data.temperature.current);
+  let cityElement = document.querySelector("#weather-app-city");
+  let city = response.data.city;
+
+  cityElement.innerHTML = city;
+  tempElement.innerHTML = temperature;
+}
+
+function searchCity(city) {
+  let apiKey = "3tb400c941f2a4edo23f9347ffbbacfd";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
+  axios.get(apiUrl).then(changeWeather);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#weather-app-city");
   let city = searchInput.value;
-  cityElement.innerHTML = city;
+  searchCity(city);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Tucson");

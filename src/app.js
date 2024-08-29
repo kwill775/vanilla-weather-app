8a2a4f45563e93b723bec3c17d0862d6808fd0c0
kwill775 <spn775@yahoo.com>
2024-08-28 20:35:29 -0700

    Add basic search engine, no API

---
+++ src/app.js
@@ -0,0 +1,10 @@
+function handleSearchSubmit(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-form-input");
+  let cityElement = document.querySelector("#weather-app-city");
+  let city = searchInput.value;
+  cityElement.innerHTML = city;
+}
+
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", handleSearchSubmit);
